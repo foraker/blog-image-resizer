ac46d598fc5705a8e737c5b92b6eddaa1d04d07a
Jon Evans <jle@foraker.com>
2016-01-08 14:34:24 -0700

    Add @3x for future-proofing

--- resize.rb
+++ resize.rb
@@ -1,13 +1,16 @@
 HEADER_SIZES = {
-  "<file_name>.jpg" => "820x330",
-  "<file_name>@2x.jpg" => "1640x660"
+  "<file_name>.jpg"    => "820x330",
+  "<file_name>@2x.jpg" => "1640x660",
+  "<file_name>@3x.jpg" => "2460x990"
 }
 
 BACKGROUND_SIZES = {
   "<file_name>-mobile.jpg"    => "480x475",
   "<file_name>-mobile@2x.jpg" => "960x950",
+  "<file_name>-mobile@3x.jpg" => "1440x1425",
   "<file_name>.jpg"           => "1440x640",
-  "<file_name>@2x.jpg"        => "2880x1280"
+  "<file_name>@2x.jpg"        => "2880x1280",
+  "<file_name>@3x.jpg"        => "4320x1920"
 }
 
 file_name = ARGV[0]
